b823715c217b9689b484328d85e9a5c9191ab0f8
Mikael Berthe <mikael@lilotux.net>
2006-01-07 22:42:08 +0100

    Close standard fds before launching the eventcmd process

--- mcabber/src/hooks.c
+++ mcabber/src/hooks.c
@@ -324,6 +324,10 @@ void hk_ext_cmd(const char *jid, guchar type, guchar info, const char *data)
   }
 
   if (pid == 0) { // child
+    // Close standard file descriptors
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
     if (execl(extcmd, extcmd, arg_type, arg_info, jid, arg_data, NULL) == -1) {
       // scr_LogPrint(LPRINT_LOGNORM, "Cannot execute external command.");
       exit(1);

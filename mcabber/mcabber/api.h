d10d4938c0dedd05732c0bcfc7f82a3a4ee59a25
Mikael Berthe <mikael@lilotux.net>
2011-03-14 13:07:55 +0100

    Update API ChangeLog (v17)

--- mcabber/mcabber/api.h
+++ mcabber/mcabber/api.h
@@ -3,7 +3,7 @@
 
 #include <mcabber/config.h> // For MCABBER_BRANCH
 
-#define MCABBER_API_VERSION 16
+#define MCABBER_API_VERSION 17
 #define MCABBER_API_MIN     13
 
 extern const gchar *mcabber_branch;

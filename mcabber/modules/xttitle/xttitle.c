a408a41cb34970785a582155b31c04d1afdf85ce
Myhailo Danylenko <isbear@ukrpost.net>
2012-11-22 00:59:19 +0200

    Add missing stdio.h to xttitle.c

--- mcabber/modules/xttitle/xttitle.c
+++ mcabber/modules/xttitle/xttitle.c
@@ -20,6 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <mcabber/modules.h>
 #include <mcabber/settings.h>

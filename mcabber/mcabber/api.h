748d4117f1ffa5a5b88fc6c8c503f5ad2b1708f6
Mikael Berthe <mikael@lilotux.net>
2014-05-08 17:16:33 +0200

    Update API (ssl_ciphers, ssl_ca, color_timestamp)

--- mcabber/mcabber/api.h
+++ mcabber/mcabber/api.h
@@ -4,7 +4,7 @@
 #include <glib.h>
 #include <mcabber/config.h> // For MCABBER_BRANCH
 
-#define MCABBER_API_VERSION 27
+#define MCABBER_API_VERSION 28
 #define MCABBER_API_MIN     27
 
 #define MCABBER_BRANCH_DEV  1

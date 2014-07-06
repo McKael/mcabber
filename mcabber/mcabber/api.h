c8f51c2e6c664ebc40f2f4203f211f81c6745d9b
Mikael Berthe <mikael@lilotux.net>
2014-07-06 14:51:14 +0200

    Update API (hook hook-mdr-received)

--- mcabber/mcabber/api.h
+++ mcabber/mcabber/api.h
@@ -4,7 +4,7 @@
 #include <glib.h>
 #include <mcabber/config.h> // For MCABBER_BRANCH
 
-#define MCABBER_API_VERSION 31
+#define MCABBER_API_VERSION 32
 #define MCABBER_API_MIN     27
 
 #define MCABBER_BRANCH_DEV  1

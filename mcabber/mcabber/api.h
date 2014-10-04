2e2923405186f27ea85b37c6bc9f20b09cb3d376
Mikael Berthe <mikael@lilotux.net>
2014-10-04 20:45:36 +0200

    Update API - hk_message_in()

--- mcabber/mcabber/api.h
+++ mcabber/mcabber/api.h
@@ -4,8 +4,8 @@
 #include <glib.h>
 #include <mcabber/config.h> // For MCABBER_BRANCH
 
-#define MCABBER_API_VERSION 34
-#define MCABBER_API_MIN     33
+#define MCABBER_API_VERSION 35
+#define MCABBER_API_MIN     35
 
 #define MCABBER_BRANCH_DEV  1
 
